eaef2ecad1ecae3d67d4571a1d99633115086507
shiwei <shiwei.li@visenze.com>
2021-01-19 20:43:41 +0800

    fix bug for list parameters

--- visearch-android/src/main/java/com/visenze/visearch/android/network/APIService.java
+++ visearch-android/src/main/java/com/visenze/visearch/android/network/APIService.java
@@ -3,6 +3,7 @@ package com.visenze.visearch.android.network;
 import com.visenze.visearch.android.model.ResponseData;
 import com.visenze.visearch.android.network.retry.Retry;
 
+import java.util.List;
 import java.util.Map;
 
 import okhttp3.MultipartBody;
@@ -14,40 +15,41 @@ import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.PartMap;
+import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
 public interface APIService {
     @Retry
     @GET("search")
-    Call<ResponseData> search(@HeaderMap Map<String, String> headers, @QueryMap Map<String, String> query);
+    Call<ResponseData> search(@HeaderMap Map<String, String> headers, @QueryMap RetrofitQueryMap query);
 
     @Retry
     @GET("recommendation")
-    Call<ResponseData> recommendation(@HeaderMap Map<String, String> headers, @QueryMap Map<String, String> query);
+    Call<ResponseData> recommendation(@HeaderMap Map<String, String> headers, @QueryMap RetrofitQueryMap query);
 
     @Retry
     @GET("colorsearch")
-    Call<ResponseData> colorSearch(@HeaderMap Map<String, String> headers, @QueryMap Map<String, String> query);
+    Call<ResponseData> colorSearch(@HeaderMap Map<String, String> headers, @QueryMap RetrofitQueryMap query);
 
     @Retry
     @GET("uploadsearch")
-    Call<ResponseData> uploadSearch(@HeaderMap Map<String, String> headers, @QueryMap Map<String, String> query);
+    Call<ResponseData> uploadSearch(@HeaderMap Map<String, String> headers, @QueryMap RetrofitQueryMap query);
 
     @Retry
     @Multipart
     @POST("uploadsearch")
-    Call<ResponseData> uploadSearch(@HeaderMap Map<String, String> headers, @Part MultipartBody.Part image, @PartMap Map<String, RequestBody> query);
+    Call<ResponseData> uploadSearch(@HeaderMap Map<String, String> headers, @Part MultipartBody.Part image, @QueryMap RetrofitQueryMap query);
+
 
     // unlike uploadSearch accept GET method if im_url
     // discoversearch accept post type if im_url is set...
     @Retry
     @POST("discoversearch")
-    Call<ResponseData> discoverSearch(@HeaderMap Map<String, String> headers, @QueryMap Map<String, String> query);
+    Call<ResponseData> discoverSearch(@HeaderMap Map<String, String> headers, @QueryMap RetrofitQueryMap query);
 
     @Retry
     @Multipart
     @POST("discoversearch")
-    Call<ResponseData> discoverSearch(@HeaderMap Map<String, String> headers, @Part MultipartBody.Part image, @PartMap Map<String, RequestBody> query);
-
+    Call<ResponseData> discoverSearch(@HeaderMap Map<String, String> headers, @Part MultipartBody.Part image, @QueryMap RetrofitQueryMap query);
 
 }
